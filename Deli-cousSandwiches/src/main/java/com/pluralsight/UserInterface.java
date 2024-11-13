e27afee99d9b3b7de88c782f1b5c427ff16a6c9a
mruzomberka311 <gmruz311@gmail.com>
2024-11-13 13:33:33 -0500

    Created display menus for sandwich options

--- Deli-cousSandwiches/src/main/java/com/pluralsight/UserInterface.java
+++ Deli-cousSandwiches/src/main/java/com/pluralsight/UserInterface.java
@@ -5,26 +5,43 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class UserInterface {
+public class UserInterface
+{
 Scanner scanner = new Scanner(System.in);
 Order order = new Order();
 
 
 
-public void displayMenu(){
+public void displayMenu()
+{
     System.out.println("Welcome to the Krusty Krab my name is Squidwarddd. How may I help you?");
 
-    int choice = scanner.nextInt();
+
     System.out.println("What is your name?");
     String customerName = scanner.nextLine();
 
+boolean running = true;
 
+while (running)
+{
+    System.out.println("Main menu");
+    System.out.println("1) Order Sandwich");
+    System.out.println("2) Order Drink");
+    System.out.println("3) Order Chips");
+    System.out.println("4) Checkout");
+    System.out.println("0) Cancel Order");
+
+    int choice = scanner.nextInt();
 
 
     //display the menu
 
-    switch (choice){
+    switch (choice)
+    {
         case 1:
+
+
+
             System.out.println("What size sandwich would you like?");
             String size = scanner.nextLine();
             System.out.println("What type of bread would you like?");
@@ -52,7 +69,7 @@ public void displayMenu(){
             String drinkFlavor = scanner.nextLine();
             System.out.println("What size drink would you like? small/medium/large");
             String drinkSize = scanner.nextLine();
-            Drinks drink = new Drinks(drinkSize,drinkFlavor);
+            Drinks drink = new Drinks(drinkSize, drinkFlavor);
             drink.getPrice(drinkSize);
             List<Drinks> drinksList = new ArrayList<>();
             drinksList.add(drink);
@@ -72,37 +89,67 @@ public void displayMenu(){
             //checkout
 
 
-
             break;
-        case 0 :
+        case 0:
             //cancel order
-          sandwichList.clear();
-          drinksList.clear();
-          chipsList.clear();
-          return;
-            break;
+            sandwichList.clear();
+            drinksList.clear();
+            chipsList.clear();
+            running = false;
+        break;
         default:
             System.out.println("Please choose an option listed above.");
             break;
     }
 }
+}
 
 public void displayBreadMenu(){
-
+    System.out.println("/n Bread Menu");
+    System.out.println("1) white");
+    System.out.println("2) wheat");
+    System.out.println("3) rye");
+    System.out.println("4) wrap");
 
 }
 public void displayMeatMenu(){
-
+    System.out.println("/n Meat Menu");
+    System.out.println("1) steak");
+    System.out.println("2) ham");
+    System.out.println("3) salami");
+    System.out.println("4) roast beef");
+    System.out.println("5) chicken");
+    System.out.println("6) bacon");
 }
 public void displayCheeseMenu(){
+    System.out.println("/n Cheese Menu");
+    System.out.println("1) american");
+    System.out.println("2) provolone");
+    System.out.println("3) cheddar");
+    System.out.println("4) swiss");
 
 }
 
 public void displayRegularToppingsMenu(){
-
+    System.out.println("/n Regular Toppings Menu");
+    System.out.println("1) lettuce");
+    System.out.println("2) peppers");
+    System.out.println("3) onions");
+    System.out.println("4) tomatoes");
+    System.out.println("5) jalepenos");
+    System.out.println("6) cucumbers");
+    System.out.println("7) pickles");
+    System.out.println("8)guacamole");
+    System.out.println("9) mushrooms");
 }
 public void displaySauceMenu(){
-
+    System.out.println("/n Sauce Menu");
+    System.out.println("1) mayo");
+    System.out.println("2) mustard");
+    System.out.println("3) ketchup");
+    System.out.println("4) ranch");
+    System.out.println("5) thousand island");
+    System.out.println("6) vinaigrette");
 }
 
 public void addOrder(){
