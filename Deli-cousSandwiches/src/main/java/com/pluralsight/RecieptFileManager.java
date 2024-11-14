1ad3403b9a58123084b1eb5907848fc5a5e3640a
mruzomberka311 <gmruz311@gmail.com>
2024-11-14 13:30:29 -0500

    printReceipt method complete

--- Deli-cousSandwiches/src/main/java/com/pluralsight/RecieptFileManager.java
+++ Deli-cousSandwiches/src/main/java/com/pluralsight/RecieptFileManager.java
@@ -1,14 +1,38 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 public class RecieptFileManager {
 
     public void saveOrder(){
 
     }
 
-    public void printReciept(Order order){
+    public void printReciept(Order order)
+    {
+        File file = new File("receipts.txt");
+        try
+        {
+            if (!file.exists())
+            {
+                file.createNewFile();
+            } else
+            {
+                FileWriter fileWriter = new FileWriter("receipts.txt");
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                bufferedWriter.write(order.toString());
+                bufferedWriter.close();
+                System.out.println("Thank you for your money! Have a good day!");
+            }
+        } catch (Exception e) {
+            System.err.println("Error creating file");
+            e.printStackTrace();
+        }
+
+
+
 
     }
-
-
-}
